d8b6748fefd0a14ffc52e1c1723e6775f25adef0
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2025-11-03 22:26:28 -0600

    Querying in nuxt content

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>All the blog list</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+// queryContent(/blog) comes from the pages folder (only if they have <ContentDoc />)
+const { data: posts } = await useAsyncData('blog-list', () => queryContent('/blog').only(['_path', 'title']).find());
+</script>
