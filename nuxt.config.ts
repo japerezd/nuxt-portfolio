f08678411b2ed7a61917ddccc78098a9e6ced5c8
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2025-11-03 22:10:47 -0600

    Add another page and add code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/content']
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/content'],
+  content: {
+    highlight: {
+      theme: {
+        default: 'night-owl',
+      }
+    }
+  }
 })
